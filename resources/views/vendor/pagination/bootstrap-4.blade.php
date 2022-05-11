e4171a37e9faae32ca8684d9e644e3ddef68ab40
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-05-11 17:09:32 +0200

    tolta freccia nel caso non ci sia una pagina prima o dopo da visualizzare

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -4,7 +4,6 @@
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
                 </li>
             @else
                 <li class="page-item">
@@ -36,7 +35,6 @@
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
                 </li>
             @endif
         </ul>
